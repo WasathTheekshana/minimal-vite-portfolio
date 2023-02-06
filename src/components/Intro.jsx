628b90e84f165cf1f1fe2c78eef6d7ed256b570b
WasathTheekshana <wasath.vt@gmail.com>
2023-02-06 21:33:45 +0530

    test: CICD

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -14,7 +14,7 @@ function Intro() {
             <h1 className={style.heading}>Wasath</h1>
             <p className={style.paragraphOne}>Software Engineer & Web Developer</p>
             <p className={style.paragraphTwo}>
-                Wasath is a highly motivated and dedicated professional who possesses a unique set of skills and experiences. He is skilled in software engineering and web development, which makes him an asset to any team working on a software project. Additionally, his background in UI/UX design showcases his ability to create intuitive and user-friendly interfaces.
+                {/* Wasath is a highly motivated and dedicated professional who possesses a unique set of skills and experiences. He is skilled in software engineering and web development, which makes him an asset to any team working on a software project. Additionally, his background in UI/UX design showcases his ability to create intuitive and user-friendly interfaces. */}
                 <br />
                 His current studies in software engineering are a testament to his commitment to continuous learning and development in the field. This, combined with his interest in cutting-edge technologies such as cloud computing and edge computing, makes Wasath well-positioned to keep abreast of the latest advancements in the industry.
             </p>
